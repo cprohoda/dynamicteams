6a68d746fee629a8945f99aaeea14bfceaa994f7
Chris Prohoda <cgprohoda@gmail.com>
2018-10-11 15:29:54 -0400

    Functionality for creating and showing current users

--- src/database.rs
+++ src/database.rs
@@ -1,10 +1,35 @@
 use diesel::pg::PgConnection;
+use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
-fn connect() -> PgConnection {
-	dotenv.ok();
+use models::{User, NewUser};
 
-	let database_url = env::var("DATABASE_URL")?;
-	PgConnection::establish(&database_url)?
+pub fn connect() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("Cannot find DATABASE_URL in .env file");
+    PgConnection::establish(&database_url).expect("Error connecting to database")
+}
+
+pub fn show_users(connection: &PgConnection) {
+    use schema::users::dsl::*;
+    let users_data = users.load::<User>(connection).expect("blah");
+
+    for user in users_data {
+        println!("{:?}", user.id);
+        println!("{:?}", user.email);
+        println!("{:?}", user.skills);
+        println!("{:?}", user.tasks);
+        println!("\n");
+    }
+}
+
+pub fn create_user(connection: &PgConnection, user: &NewUser) {
+    use schema::users::dsl::*;
+
+    diesel::insert_into(users)
+        .values(user)
+        .get_result::<User>(connection)
+        .expect("Error saving new users");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
-extern crate rocket;
+#[macro_use]
 extern crate diesel;
 extern crate dotenv;
+extern crate rocket;
+
+mod database;
+mod models;
+mod schema;
+
+use diesel::prelude::*;
+use database::{connect, create_user, show_users};
+use models::NewUser;
 
 #[get("/")]
 fn index() -> &'static str {
-	"Hello world"
+    "Hello world"
 }
 
 fn main() {
-	rocket::ignite().mount("/", routes![index]).launch();
+    let connection = connect();
+
+    let user1 = NewUser {
+        email: "blah@fake.com",
+        tasks: vec!["bugfix1","newfeature1","improvement1","improvement2"],
+        skills: vec!["software development"]
+    };
+
+    let user2 = NewUser {
+        email: "yah@fake.com",
+        tasks: vec!["bugfix2", "bugfix3"],
+        skills: vec!["bug fixing"],
+    };
+
+    create_user(&connection, &user1);
+    create_user(&connection, &user2);
+
+    show_users(&connection);
+
+    rocket::ignite().mount("/", routes![index]).launch();
 }
