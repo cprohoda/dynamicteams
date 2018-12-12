154def93b8e399149a93e7943a44151b1ef07a16
Chris Prohoda <cgprohoda@gmail.com>
2018-12-11 20:08:58 -0500

    Refactor main function routine into database unit tests

--- src/database.rs
+++ src/database.rs
@@ -45,3 +45,76 @@ pub fn update_user(connection: &PgConnection, user: &User) -> Result<User, diese
              users::tasks.eq(&user.tasks)))
         .get_result::<User>(connection)
 }
+
+
+mod tests {
+    #[test]
+    fn create_user_test() {
+        use database::connect;
+        use diesel::prelude::*;
+        use models::{User,NewUser};
+        use database::create_user;
+
+        let connection = connect();
+
+        let test_user = NewUser {
+            email: "iuhewdiunasidai@fake.com",
+            tasks: vec!["bugfix1","improvement1"],
+            skills: vec!["software development"],
+        };
+
+        let expected_test_user = User {
+            id: 1,
+            email: "iuhewdiunasidai@fake.com".to_string(),
+            tasks: Some(vec!["bugfix1".to_string(),"improvement1".to_string()]),
+            skills: Some(vec!["software development".to_string()]),
+        };
+
+        connection.test_transaction::<_, diesel::result::Error, _>(|| {
+            let created_test_user = create_user(&connection, &test_user).expect("Test user creation failed");
+
+            if expected_test_user.email != created_test_user.email &&
+               expected_test_user.tasks != created_test_user.tasks &&
+               expected_test_user.skills != created_test_user.skills {
+                Err(diesel::result::Error::__Nonexhaustive)
+            } else {
+                Ok(())
+            }
+        })
+    }
+
+    #[test]
+    fn repeat_create_test() {
+        use database::connect;
+        use diesel::prelude::*;
+        use models::NewUser;
+        use database::create_user;
+
+        let connection = connect();
+
+        let test_user = NewUser {
+            email: "iojdoiajsoidjascod@fake.com",
+            tasks: vec!["bugfix1","improvement1"],
+            skills: vec!["software development"],
+        };
+
+        connection.test_transaction::<_, diesel::result::Error, _>(|| {
+            let first_create_test = create_user(&connection, &test_user);
+            let second_create_test = create_user(&connection, &test_user);
+
+            if first_create_test.is_err() && second_create_test.is_err() {
+                Err(diesel::result::Error::__Nonexhaustive)
+            } else {
+                Ok(())
+            }
+        })
+    }
+
+    // #[test]
+    // fn get_user_test() {
+    // }
+
+    // #[test]
+    // fn update_user_test() {
+    // }
+}
